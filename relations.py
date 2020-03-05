5adb5576e8c518d6aade80c909bdd8222f26cb83
Richard Biro <biro.risko@gmail.com>
2020-03-05 22:09:40 +0100

    pridane metody union, intersect, subtract, reflexive, symmetric, transitive

--- relations.py
+++ relations.py
@@ -18,5 +18,24 @@ class Relation:
             return Relation(self.M,self.rel.remove((a,b)))
         return self
 
+    def union(self,R):
+        return Relation(self.M,self.rel.union(R.rel))
+
+    def intersect(self,R):
+        return Relation(self.M,self.rel.intersection(R.rel))
+
+    def subtract(self,R):
+        return Relation(self.M,self.rel.difference(R.rel))
+
+    def reflexive(self):
+        return all(self.contains(a,a) for a in self.M)
+
+    def symmetric(self):
+        return all(self.contains(b,a) for (a,b) in self.rel)
+
+    def transitive(self):
+        return all(self.contains(a,c) or b1 != b2
+                   for (a,b1) in self.rel for (b2,c) in self.rel)
+
 def get_relation_class(M):
     return Relation(M)
