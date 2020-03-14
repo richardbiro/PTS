c35f8048221974c6b479b2ee0b9bb5dc0c89d5de
Richard Biro <biro.risko@gmail.com>
2020-03-14 18:01:49 +0100

    upravene (v ramci moznosti) podla code review

--- relations.py
+++ relations.py
@@ -1,79 +1,80 @@
 from pyrsistent import s, freeze
+import warnings
 
-
-class Relation:
-    #ak do initu dostanes prvykrat mutable set, tak bude mutable
-    #ak berieme ze objekty nemusia byt immutable
-    #lebo pri kazdej zmene sa meni instancia
-    #tak nemusis mat freeze v inverse a composition
-    def __init__(self,M,rel = s()):
-        self.M = M 
-        self.rel = rel
-        
-    def contains(self,a,b):
-        return (a,b) in self.rel
-
-    #pri metode add by som vypisala warning, ak prvok nepatri do mnoziny relacie
-    def add(self,a,b):
-        if a in self.M and b in self.M:
-            return Relation(self.M,self.rel.add((a,b)))
-        return self
-
-    def remove(self,a,b):
-        if self.contains(a,b):
-            return Relation(self.M,self.rel.remove((a,b)))
-        return self
-
-    #zjednot aj mnoziny relacii
-    def union(self,R):
-        return Relation(self.M,self.rel.union(R.rel))
-
-    def intersect(self,R):
-        return Relation(self.M,self.rel.intersection(R.rel))
-
-    def subtract(self,R):
-        return Relation(self.M,self.rel.difference(R.rel))
-
-    def inverse(self):
-        return Relation(self.M,
-                        freeze(set(x[::-1] for x in self.rel)))
-
-    def composition(self,R):
-        return Relation(self.M.union(R.M),
-                        freeze(set( (a,c) for (a,b1) in R.rel
-                                    for (b2,c) in self.rel if b1 == b2)))
-
-    def reflexive(self):
-        return all(self.contains(a,a) for a in self.M)
-
-    def symmetric(self):
-        return all(self.contains(b,a) for (a,b) in self.rel)
-
-    def transitive(self):
-        return all(self.contains(a,c) or b1 != b2
-                   for (a,b1) in self.rel for (b2,c) in self.rel)
-
-    #nebolo by viac efektivne vytvorit si docasny set?
-    def closure(self):
-        R = Relation(self.M,freeze(set((a,a) for a in self.M)).union(self.rel))
-        while not R.composition(self).rel.issubset(R.rel):
-            R = R.union(R.composition(self))
-        return R
-
-    def printrel(self,text):
-        print()
-        print(text,"=",self.rel)
-        print("reflexivna?",self.reflexive())
-        print("symetricka?",self.symmetric())
-        print("tranzitivna?",self.transitive())
-
-
-#funkcia get_relation_class(M) vracia instanciu triedy, nie triedu
 def get_relation_class(M):
-    return Relation(M)
+    class Relation:
+        rset = M
+        
+        def __init__(self,rel = s()):
+            self.rel = rel
+            
+        def contains(self,a,b):
+            return (a,b) in self.rel
+
+        def add(self,a,b):
+            if a in self.rset and b in self.rset:
+                return Relation(self.rel.add((a,b)))
+            warnings.warn("Error: both a,b must be in the set of the relation!")
+            return self
+
+        def remove(self,a,b):
+            if self.contains(a,b):
+                return Relation(self.rel.remove((a,b)))
+            warnings.warn("Error: (a,b) not found in relation!")
+            return self
+
+        def union(self,R):
+            if R.rset.issubset(self.rset):
+                return Relation(self.rel.union(R.rel))
+            warnings.warn("Error: relations are not on the same set!")
+            return self
+
+        def intersect(self,R):
+            return Relation(self.rel.intersection(R.rel))
+
+        def subtract(self,R):
+            return Relation(self.rel.difference(R.rel))
+
+        def inverse(self):
+            return Relation(set(x[::-1] for x in self.rel))
+
+        def composition(self,R):
+            if R.rset.issubset(self.rset):
+                return Relation(set( (a,c) for (a,b1) in R.rel
+                                 for (b2,c) in self.rel if b1 == b2))
+            warnings.warn("Error: relations are not on the same set!")
+            return self
+            
+
+        def reflexive(self):
+            return all(self.contains(a,a) for a in self.rset)
+
+        def symmetric(self):
+            return all(self.contains(b,a) for (a,b) in self.rel)
+
+        def transitive(self):
+            return all(self.contains(a,c) or b1 != b2
+                       for (a,b1) in self.rel for (b2,c) in self.rel)
+
+        def closure(self):
+            R = Relation(freeze(set((a,a) for a in self.rset)).union(self.rel))
+            while not R.composition(self).rel.issubset(R.rel):
+                R = R.union(R.composition(self))
+            return R
+
+        def printrel(self,text):
+            print()
+            print(text,"=",self.rel)
+            print("reflexivna?",self.reflexive())
+            print("symetricka?",self.symmetric())
+            print("tranzitivna?",self.transitive())
+            
+    return Relation
 
 
-a = get_relation_class({1,2,3,4})
+P = get_relation_class({1,2,3,4})
+
+a = P()
 a = a.add(1,1)
 a = a.add(1,3)
 a = a.add(2,2)
@@ -88,7 +89,10 @@ a2.printrel("A^(-1)")
 a3 = a.closure()
 a3.printrel("A*")
 
-b = get_relation_class({1,2,3})
+
+Q = get_relation_class({1,2,3})
+
+b = Q()
 b = b.add(1,2)
 b = b.add(1,3)
 b = b.add(2,3)
@@ -100,7 +104,10 @@ ab.printrel("A o B")
 asb = a.subtract(b)
 asb.printrel("A - B")
 
-c = get_relation_class({1,2})
+
+R = get_relation_class({1,2})
+
+c = R()
 c = c.add(1,2)
 c = c.add(2,1)
 c.printrel("C")
